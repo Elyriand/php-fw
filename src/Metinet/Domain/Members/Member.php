597adff8529c4ee1a8ff8946f7e98ebcdc0db52e
Elyriand Steeve <steeve.w01@gmail.com>
2018-01-19 09:53:01 +0100

    Commit member

--- src/Metinet/Domain/Members/Member.php
+++ src/Metinet/Domain/Members/Member.php
@@ -11,18 +11,46 @@ use Metinet\Domain\Conferences\Email;
 
 class Member implements Account
 {
+    private $id;
+    private $profile;
     private $email;
     private $encodedPassword;
 
-    private function __construct(Email $email, EncodedPassword $encodedPassword)
+    private function __construct($id, Profile $profile, Email $email, EncodedPassword $encodedPassword)
     {
+        $this->id = $id;
+        $this->profile = $profile;
         $this->email = $email;
         $this->encodedPassword = $encodedPassword;
     }
 
-    public static function register(Email $email, EncodedPassword $encodedPassword): self
+    public static function register(Profile $profile, Email $email, EncodedPassword $encodedPassword): self
     {
-        return new self($email, $encodedPassword);
+        return new self(uniqid(), $profile, $email, $encodedPassword);
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            $data['id'],
+            new Profile(
+                $data['first_name'],
+                $data['last_name'],
+                'todo'
+            ),
+            new Email($data['email']),
+            new EncodedPassword($data['encoded_password'], $data['password_salt'])
+        );
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getProfile(): Profile
+    {
+        return $this->profile;
     }
 
     public function getEmail(): string
